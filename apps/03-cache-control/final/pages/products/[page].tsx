607de5d80622fbbd44e1234e7be4bf82fdeb0678
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2022-10-23 14:11:15 +0700

    feat: create cache control app

---
+++ apps/03-cache-control/final/pages/products/[page].tsx
@@ -0,0 +1,23 @@
+import { GetServerSideProps } from 'next';
+import {
+  getProductListScreenInitialProps,
+  ProductListScreen,
+  ProductListScreenProps,
+} from '@frontend-research/screens';
+
+type Params = {
+  page: string;
+};
+
+export const getServerSideProps: GetServerSideProps<
+  ProductListScreenProps,
+  Params
+> = async (ctx) => {
+  ctx.res.setHeader('Cache-Control', 'max-age=60');
+  const pageParams = ctx.params?.page ?? '1';
+  const page = parseInt(pageParams);
+  const props = await getProductListScreenInitialProps(page);
+  return { props };
+};
+
+export default ProductListScreen;
