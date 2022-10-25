114a7c500a8da3d60605cd93626fdab90dd8092c
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2022-10-25 09:49:16 +0700

    feat: styles the products page

--- apps/03-cache-control/final/pages/_app.tsx
+++ apps/03-cache-control/final/pages/_app.tsx
@@ -1,17 +1,17 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <Head>
         <title>Welcome to 01-data-fetching!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
-    </>
+    </ChakraProvider>
   );
 }
 
--- libs/screens/src/lib/product-list-screen.tsx
+++ libs/screens/src/lib/product-list-screen.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { css } from '@emotion/react';
 import Link from 'next/link';
 import { getProducts, ProductsApiResponse } from '@frontend-research/fetchers';
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 
 export type ProductListScreenProps = {
   productsResponse: ProductsApiResponse | null;
@@ -31,55 +40,109 @@ export function ProductListScreen(props: ProductListScreenProps) {
   const skip = LIMIT * (props.page - 1);
 
   React.useEffect(() => {
-    if (productsResponse === null) {
+    if (
+      props.productsResponse === null ||
+      props.productsResponse.skip !== skip
+    ) {
       setLoading(true);
       getProducts({ skip, limit: LIMIT })
         .then(setProductsResponse)
         .catch((err) => setError(err.message))
         .finally(() => setLoading(false));
     }
-  }, [productsResponse, skip]);
+  }, [props.productsResponse, skip]);
 
   return (
-    <div>
-      <h1
-        css={css`
-          color: red;
-        `}
-      >
-        Welcome
-      </h1>
-      {loading ? (
-        <p>loading...</p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : productsResponse === null ||
-        productsResponse.products.length === 0 ? (
-        <p>empty data</p>
-      ) : (
-        <>
-          <ol start={skip + 1}>
-            {productsResponse?.products.map(({ id, title }) => (
-              <li
-                key={id}
-                css={css`
-                  font-weight: bold;
-                `}
-              >
-                {title}
-              </li>
-            ))}
-          </ol>
-          <div>
-            {Array.from(Array(totalPage)).map((_, index) => (
-              <Link key={index} href={`/products/${index + 1}`}>
-                <a>{index + 1}</a>
-              </Link>
-            ))}
-          </div>
-        </>
-      )}
-    </div>
+    <Flex
+      backgroundColor="gray.100"
+      height="100vh"
+      justifyContent="center"
+      overflowY="scroll"
+    >
+      <Box maxWidth="container.xl" padding={['1', '2', '8']}>
+        <Image
+          src="https://skillacademy-prod-image.skillacademy.com/image/a29db7c7-2f2f-4be0-b04e-a72938f1d0c9.jpg"
+          marginBottom="10"
+        />
+
+        <Box marginBottom="5">
+          <Heading>Products</Heading>
+          <Text>We sell our quality products</Text>
+        </Box>
+
+        {loading ? (
+          <p>loading...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : productsResponse === null ||
+          productsResponse.products.length === 0 ? (
+          <p>empty data</p>
+        ) : (
+          <>
+            <Grid
+              columnGap="5"
+              templateColumns={[
+                '1fr',
+                'repeat(2, 1fr)',
+                'repeat(3, 1fr)',
+                'repeat(4, 1fr)',
+              ]}
+            >
+              {productsResponse?.products.map(
+                ({ id, title, price, thumbnail }) => (
+                  <GridItem key={id}>
+                    <Box borderRadius="xl" overflow="hidden">
+                      <Box
+                        backgroundImage={`url('${thumbnail}')`}
+                        backgroundPosition="center"
+                        backgroundSize="cover"
+                        width="100%"
+                        height="52"
+                      />
+                      <Box
+                        padding="5"
+                        borderRadius="xl"
+                        backgroundColor="white"
+                        position="relative"
+                        bottom="5"
+                      >
+                        <Heading size="md" noOfLines={1}>
+                          {title}
+                        </Heading>
+                        <Text marginBottom="5">$ {price}</Text>
+                        <Button
+                          width="100%"
+                          backgroundColor="blue.500"
+                          color="white"
+                        >
+                          Buy
+                        </Button>
+                      </Box>
+                    </Box>
+                  </GridItem>
+                )
+              )}
+            </Grid>
+
+            <Flex flexWrap="wrap">
+              {Array.from(Array(totalPage)).map((_, index) => (
+                <Box key={index} marginBottom="5" marginRight="5">
+                  <Link href={`/products/${index + 1}`}>
+                    <Button
+                      disabled={props.page === index + 1}
+                      backgroundColor="gray.300"
+                      _hover={{ backgroundColor: 'gray.500', color: 'white' }}
+                    >
+                      {index + 1}
+                    </Button>
+                  </Link>
+                </Box>
+              ))}
+            </Flex>
+          </>
+        )}
+      </Box>
+    </Flex>
   );
 }
 
